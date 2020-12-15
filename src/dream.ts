1638b97078e18503f282a3604c89003d3e285401
Henrique Cunha <henriquecunhawd@gmail.com>
2020-12-15 00:26:56 -0300

    [COMMON_WORDS]: Add common words feature

--- src/dream.ts
+++ src/dream.ts
@@ -1,12 +1,16 @@
 import { Arango } from "./arango";
+import * as stopword from "stopword";
+import { aql } from "arangojs";
 
 export class Dream {
     body: string | null;
+    bodyClean: string[] | null;
     user: string | null;
     date: string | null;
 
     constructor(params: IDreamConstructor) {
         this.body = params.body || null;
+        this.bodyClean = Dream.clearBody(params.body);
         this.user = params.user || null;
         this.date = new Date().toISOString();
     }
@@ -20,6 +24,30 @@ export class Dream {
             collection: "dream-log",
             condition: `item.user == '${user}'`,
         });
+    }
+
+    static async getCommonWordsFromUser(
+        user: string,
+        limit = 10
+    ): Promise<{ word: string; count: number }[]> {
+        const results = await Arango.query(aql`
+            let words = flatten(
+                for dream in \`dream-log\`
+                    filter dream.user == '${aql.literal(user)}'
+                    return dream.bodyClean
+            )
+            
+            for wordOcc in words
+                collect word = wordOcc with count into \`count\`
+                sort \`count\` desc
+                limit ${aql.literal(limit)}
+                return { word, \`count\` }
+        `);
+        return results;
+    }
+
+    private static clearBody(body: string | null) {
+        return stopword.removeStopwords(String(body).split(" "), stopword.ptbr);
     }
 }
 
